6502ee3788014e6dee41f6ec01890ccb9a14cf7d
yuiseo <97578465+yuiseo@users.noreply.github.com>
2023-07-03 23:47:33 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.47 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = []
+    dict = {}
+
+    for idx, word in enumerate(s):
+        if word not in dict:
+            answer.append(-1)
+            dict[word] = idx
+            
+        else:
+            answer.append(idx-dict[word])
+            dict[word] = idx
+            
+        
+    return answer
