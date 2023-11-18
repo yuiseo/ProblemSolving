76b2705be68459fddbf873f295c12e511e0f8046
yuiseo <97578465+yuiseo@users.noreply.github.com>
2023-11-18 22:38:17 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+
+arr = []
+def dfs(start):
+    if len(arr) == M:
+        print(' '.join(map(str,arr)))
+        return
+    
+    for i in range(start,N+1):
+        arr.append(i)
+        dfs(i)
+        arr.pop()
+
+dfs(1)
