bb987663851407637fac7a4b3cb9bf3378ad4dca
yu iseo <97578465+yuiseo@users.noreply.github.com>
2025-08-15 01:08:13 +0900

    [Silver II] Title: 나무 자르기, Time: 460 ms, Memory: 258360 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,47 @@
 import sys
-from collections import Counter
+input = sys.stdin.readline
 
-# 입력
-n, m = map(int, sys.stdin.readline().split())
-trees = Counter(map(int, sys.stdin.read().split()))
+n,m = map(int,input().split())
+trees = list(map(int,input().split()))
 
-# 초기 설정
-s = 1
-e = 1000000000
+# 정렬
+trees.sort()
 
-while s <= e:
-    mid = (s + e) // 2  # 중간값 설정
-    # 중간값을 기준으로 잘랐을 때 가져갈 수 있는 나무 길이 합 계산
-    tot = sum((h - mid) * i for h, i in trees.items() if h > mid)
+# left, right
+left, right = 0, trees[-1]
+answer = 0
 
-    if tot >= m:  # 가져갈 수 있는 나무 길이 합이 목표보다 크거나 같은 경우
-        s = mid + 1  # 높이를 높여야 함
-    elif tot < m:  # 가져갈 수 있는 나무 길이 합이 목표보다 작은 경우
-        e = mid - 1  # 높이를 낮춰야 함
+while left<=right:
+    mid = (left+right)//2
+    cut = 0
 
-print(e)  # 결과 출력
+    # mid 값으로 나무를 자르기
+    # 잘랐을 때 합이 m 보다 크면 right를 mid-1로 바꿔치기
+    # 잘랐을 때 합이 m 보다 작으면 left를 mid+1로 바꿔치기
+
+    for tree in trees:
+        if tree - mid > 0:
+            cut += (tree - mid)
+
+    if cut >= m:
+        left = mid+1
+        answer = mid
+
+    # 잘라진 나무와 목표m 같을 때
+    # if cut == m:
+    #     print(mid)
+    #     break
+
+    if cut < m:
+        right = mid-1
+
+print(answer)
+
+
+
+
+
+
+
+
+
