566bd19291951c80faacb176fda00d12b4ecea8c
yu iseo <97578465+yuiseo@users.noreply.github.com>
2026-01-07 22:16:30 +0900

    [level 2] Title: 가장 큰 수, Time: 80.61 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 47.1 MB, 시간: 248.54 ms
+메모리: 45.9 MB, 시간: 80.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 07일 22:12:09
+2026년 01월 07일 22:16:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,7 +1,14 @@
 function solution(numbers) {
     var answer = ''
     const strNumbers = numbers.map((number)=>number+'')
-    strNumbers.sort((a, b) => (b + a).localeCompare(a + b));
+    // strNumbers.sort((a, b) => (b + a).localeCompare(a + b));
+    
+    strNumbers.sort((a,b)=>{
+        const ab = a+b
+        const ba = b+a
+        return ba>ab?1:-1
+    })
+    
     if (strNumbers[0] === "0") return "0";
     answer = strNumbers.join('')
     return answer;
